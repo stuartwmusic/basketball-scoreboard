e8530e3beba96a491c37f30665705aa68c2bb3c1
UniFlyer <136006180+stuartwmusic@users.noreply.github.com>
2024-01-01 15:37:01 +0700

    2nd try, revised files
    seems first try was files from wrong project folder on my HD.

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let add = document.getElementById("home-score")
+let addG = document.getElementById("guest-score")
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+let homeScore = 0
+let guestScore = 0
+ 
+function add1Home() {
+    homeScore +=1
+    add.textContent = homeScore
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+function add2Home() {
+    homeScore +=2
+    add.textContent = homeScore   
+}
+
+function add3Home() {
+    homeScore +=3
+    add.textContent = homeScore  
+}
+
+function add1Guest() {
+    guestScore +=1
+    addG.textContent = guestScore   
+}
+
+function add2Guest() {
+    guestScore +=2
+    addG.textContent = guestScore   
+}
+
+function add3Guest() {
+    guestScore +=3
+    addG.textContent = guestScore   
 }
